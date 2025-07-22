f35a2b7783ae57fbcaff429e2745840a58c5adc5
Anh Nguyet Vu <32753274+anngvu@users.noreply.github.com>
2025-07-22 21:56:51 +0000

    Test new image

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pip3 install schematicpy==24.7.2 linkml==v1.8.1 jsonata-python
+pip install schematicpy==25.7.1 linkml==v1.8.1 jsonata-python
 npm install -g json-dereference-cli
 sudo wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 && sudo chmod a+x /usr/local/bin/yq
 sudo bash < <(curl -s https://raw.githubusercontent.com/babashka/babashka/master/install)
